65c0b24c654d331a2d85b5369866e44a4cc9d13e
tom-jr <antonio.carlos@postoaki.com.br>
2023-07-29 07:59:04 -0300

    #0010

--- project-lab/data-binding/src/app/app.module.ts
+++ project-lab/data-binding/src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import {NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent
   ],
-  imports: [
-    BrowserModule,
-    NgOptimizedImage
-  ],
+    imports: [
+        BrowserModule,
+        NgOptimizedImage,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- project-lab/data-binding/src/app/data-binding/data-binding.component.ts
+++ project-lab/data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,6 +9,18 @@ export class DataBindingComponent {
 
   url: string = 'https://www.loaine.com';
   urlImage: string = 'http://lorempixel.com.br/500/400/?1';
+  aula = 2;
+  options = [
+    {label: 'Primary', value: 'alert-primary'},
+    {label: 'Secondary', value: 'alert-secondary'},
+    {label: 'Success', value: 'alert-success'},
+    {label: 'Danger', value: 'alert-danger'},
+    {label: 'Warning', value: 'alert-warning'},
+    {label: 'Info', value: 'alert-info'},
+    {label: 'Light', value: 'alert-light'},
+    {label: 'Dark', value: 'alert-dark'}
+  ];
+  model!: string;
 
   twoTermsSum(num1: number, num2: number): number {
     return num1 + num2;
